9b8a19986cade8fec0cff9fbbf7d63cad688cad4
Grecia Corona <grecia.corona@gmail.com>
2020-10-22 22:54:20 -0500

    Check if a number is a perfect square with approximation algorithm

---
+++ aproximacion.py
@@ -0,0 +1,17 @@
+# Check if a number is a perfect square with approximation algorithm
+
+objetivo = int(input('Escoje un número: '))
+epsilon = 0.01
+paso = epsilon**2
+respuesta = 0.0
+
+while abs(respuesta**2 - objetivo) >= epsilon and respuesta <= objetivo:
+    print(abs(respuesta**2 - objetivo), respuesta)
+    respuesta += paso
+
+if abs(respuesta**2 - objetivo) >= epsilon:
+    r = respuesta**2 - objetivo
+    print(f'No se encontró la raíz cuadrada del {objetivo}')
+else:
+    r = respuesta**2 - objetivo
+    print(f'La raíz cuadrada del {objetivo} es {respuesta}')
